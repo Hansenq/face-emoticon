51d83651242a014f49337646430f78f841bdec64
yichengsun <yichengs@princeton.edu>
2015-01-11 02:00:15 -0500

    added ck+ test scaled feature vector

--- clmtrackr/load_data.py
+++ clmtrackr/load_data.py
@@ -1,5 +1,5 @@
 import webbrowser, time
 
-for i in range(164, 264):
+for i in range(0, 62):
     webbrowser.open('http://127.0.0.1:8000/clmtrackr/feature_detection.html?num=' + str(i), new=2, autoraise=False)
     time.sleep(3)
