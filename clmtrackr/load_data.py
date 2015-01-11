21e6b4570ba0159da8d7759f6cdf4775ee6f47f8
Hansen Qian <hansenq@gmail.com>
2015-01-11 00:44:25 -0500

    Added javascript to read query parameter

--- clmtrackr/load_data.py
+++ clmtrackr/load_data.py
@@ -1,3 +1,3 @@
 import webbrowser
-for i in range(0, 2):
-	webbrowser.open('http://127.0.0.1:8000/clmtrackr/feature_detection.html?id=' + str(i), new=2, autoraise=False)
+for i in range(0, 10):
+    webbrowser.open('http://0.0.0.0:8000/clmtrackr/feature_detection.html?num=' + str(1), new=2, autoraise=False)
