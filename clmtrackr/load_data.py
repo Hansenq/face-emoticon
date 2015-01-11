104c10ad3a810ea08185b83e83c5829d3149441a
yichengsun <yichengs@princeton.edu>
2015-01-11 01:47:41 -0500

    clmtrackr dynamic load images works now

--- clmtrackr/load_data.py
+++ clmtrackr/load_data.py
@@ -1,4 +1,4 @@
 import webbrowser, time
-for i in range(40, 80):
-    webbrowser.open('http://0.0.0.0:8000/clmtrackr/feature_detection.html?num=' + str(i), new=2, autoraise=False)
-    time.sleep(5)
+for i in range(164, 264):
+    webbrowser.open('http://127.0.0.1:8000/clmtrackr/feature_detection.html?num=' + str(i), new=2, autoraise=False)
+    time.sleep(3)
