faed55476e516dad95466d577bacbc822303fa62
Hansen Qian <hansenq@gmail.com>
2015-01-11 00:49:48 -0500

    Add timeout to load_data.py

--- clmtrackr/load_data.py
+++ clmtrackr/load_data.py
@@ -1,3 +1,4 @@
-import webbrowser
+import webbrowser, time
 for i in range(0, 10):
-    webbrowser.open('http://0.0.0.0:8000/clmtrackr/feature_detection.html?num=' + str(1), new=2, autoraise=False)
+    time.sleep(5)
+    webbrowser.open('http://0.0.0.0:8000/clmtrackr/feature_detection.html?num=' + str(i), new=2, autoraise=False)
